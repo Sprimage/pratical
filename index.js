04b5b499c81a0c26bbe873d093c7ce534d0dd0d4
Adetayo Timilehin <timilehin1999@gmail.com>
2020-02-25 11:27:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var http = require('http');
 var express = require('express');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize({
+  database: 'postgres',
+  username: 'postgres',
+  password: 'password',
+  dialect: 'postgres',
+});
+
+
+sequelize
+  .authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch(err => console.error('Unable to connect to the database:', err));
+
 var app = express();
 
 app.get('/', function(req, res){
@@ -7,6 +24,6 @@ app.get('/', function(req, res){
 });
 
 
-app.listen(process.env.PORT || 3500, function(){
-	console.log('testapp is running at localhost:8080');
+app.listen(process.env.PORT || 7200, function(){
+	console.log('testapp is running at localhost:7200');
 });
